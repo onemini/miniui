76c1408c65a8472f82155ce89a7689f409069f6a
zsn <1059314550@qq.com>
2020-10-12 08:48:22 +0800

    'sojdof'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button disabled>Hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu Link</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
