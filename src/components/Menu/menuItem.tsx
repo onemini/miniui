7f355d81fb38d76ea55743fc4c0da2c7025ad35b
lmles <1059314550@qq.com>
2020-10-13 17:51:40 +0800

    new

---
+++ src/components/Menu/menuItem.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import classNames from 'classnames'
+import { MenuContext } from './menu'
+
+export interface MenuItemProps {
+    index: number;
+    disabled ?: boolean;
+    className ?: string;
+    style ?: React.CSSProperties
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props) => {
+    const { index, disabled, className, style, children } = props
+    const context = useContext(MenuContext)
+    const classes = classNames('menu-item', className, {
+        'is-disabled' : disabled,
+        'is-active' : context.index === index
+    })
+    const handleCLick = () => {
+        if (context.onSelect && !disabled) {
+            context.onSelect(index)
+        }
+    }
+    return (
+        <li className={classes} style={style} onClick={handleCLick}>
+            {children}
+        </li>
+    )
+}
+
+export default MenuItem
